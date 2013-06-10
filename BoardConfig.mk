40bc1a4f0a21f092032c801911e91eb20b307c61
Michael Bestas <mikeioannina@gmail.com>
2013-06-10 21:30:58 +0300

    zeus: little cleanup
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I4de208d93b582d4e30c93c9bc2fd14435506b2e9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,13 @@
 -include device/semc/zeus-common/BoardConfigCommon.mk
 -include vendor/semc/zeus/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/zeus/bluedroid
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 854
+TARGET_SCREEN_WIDTH := 480
+
 # Kernel
 TARGET_KERNEL_CONFIG := nAa_zeus_defconfig
 
@@ -22,11 +29,11 @@ TARGET_KERNEL_CONFIG := nAa_zeus_defconfig
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
 SOMC_CFG_SENSORS_PROXIMITY_SHARP_GP2 := yes
 
-TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
-
-BOARD_USES_STEREO_HW_SPEAKER := true
-
+# Assert
 TARGET_OTA_ASSERT_DEVICE := R800i,R800a,R800at,zeus
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/zeus/bluedroid
+# Stereo speakers
+BOARD_USES_STEREO_HW_SPEAKER := true
+
+# Touchpad
+TARGET_TOUCHPAD_INPUT_DEVICE_ID := 0x10003
