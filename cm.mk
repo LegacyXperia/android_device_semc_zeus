0997afd2349d795d6522facfc0a2b241994b92ca
Michael Bestas <mikeioannina@gmail.com>
2013-08-20 06:06:50 +0300

    zeus: switch to mini_phone
    Save 13,5MB in /system by excluding sample videos & some ringtones
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- cm.mk
+++ cm.mk
@@ -19,7 +19,18 @@ $(call inherit-product, device/semc/zeus/full_zeus.mk)
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800i BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
