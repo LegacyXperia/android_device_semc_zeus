30187f48bfbe74879f1543a11b14c53e17af8031
Michael Bestas <mikeioannina@gmail.com>
2013-10-29 18:55:01 +0200

    zeus: World phone
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- cm.mk
+++ cm.mk
@@ -15,9 +15,6 @@
 # Inherit device configuration
 $(call inherit-product, device/semc/zeus/full_zeus.mk)
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
@@ -33,7 +30,10 @@ PRODUCT_PACKAGES += \
     PhaseBeam
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=R800i BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=R800i \
+    BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" \
+    PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_zeus
