6af5e45c211ec74d0ef4af2210997336573142df
Michael Bestas <mikeioannina@gmail.com>
2014-03-11 01:56:27 +0200

    zeus: Fix device name
    Change-Id: Iac3842b296231922bc847bccf1e14ee4ea0094d9

--- cm.mk
+++ cm.mk
@@ -32,9 +32,12 @@ PRODUCT_PACKAGES += \
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=R800i \
+    PRODUCT_DEVICE=R800i \
     BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" \
     PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_zeus
-PRODUCT_DEVICE := zeus
+
+# Release name
+PRODUCT_RELEASE_NAME := zeus
