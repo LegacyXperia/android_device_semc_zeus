a70e661e2c04ef775a6a08c3917df5407c01ca9e
Michael Bestas <mikeioannina@gmail.com>
2014-04-14 02:01:34 +0300

    zeus: Use new defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/zeus/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := nAa_zeus_defconfig
+TARGET_KERNEL_CONFIG := lx_zeus_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
