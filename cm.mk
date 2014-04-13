ee65c9b5bc1e87290878b8b00d2366c88f2a2869
Michael Bestas <mikeioannina@gmail.com>
2014-04-14 02:15:06 +0300

    zeus: World phone

--- cm.mk
+++ cm.mk
@@ -15,9 +15,6 @@
 # Inherit device configuration
 $(call inherit-product, device/semc/zeus/full_zeus.mk)
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
