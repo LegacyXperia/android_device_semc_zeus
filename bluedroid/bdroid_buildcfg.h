a93b78c59cf7cfd232d647322829aa449b3ca05c
Michael Bestas <mikeioannina@gmail.com>
2014-04-16 18:42:51 +0300

    Update BT configuration for kitkat

--- bluedroid/bdroid_buildcfg.h
+++ bluedroid/bdroid_buildcfg.h
@@ -19,5 +19,9 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "Xperia Play"
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 
 #endif
