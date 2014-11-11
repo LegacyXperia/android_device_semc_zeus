304e7c680d612269a675a4f39cd5ff03ed50e1c1
Adam Lesinski <adamlesinski@google.com>
2014-11-11 11:12:01 -0800

    Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is
    encouraged, as AAPT is able to determine the fallback
    density to use if a resource of the specified density
    does not exist.
    
    Change-Id: If2b43a44593bc234118045d03bd48cd9820dc05a

--- zeus.mk
+++ zeus.mk
@@ -24,7 +24,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
 
-PRODUCT_AAPT_CONFIG := normal hdpi
+# Density
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 # Init files
