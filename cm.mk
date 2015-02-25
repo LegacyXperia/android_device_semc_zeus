161edf9cf08bf93a49b5e778b863a4e24e1bb1fb
Michael Bestas <mikeioannina@gmail.com>
2015-02-25 03:02:10 +0200

    Fix overriding device name
    Change-Id: Ic9faf6b9ccc63138746cdeb02151c76535154501

--- cm.mk
+++ cm.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=R800i \
-    PRODUCT_DEVICE=R800i \
+    TARGET_DEVICE=R800i \
     BUILD_FINGERPRINT="SEMC/R800i_0000-0000/R800i:4.0.3/4.1.H.0.4/-z9-3w:user/release-keys" \
     PRIVATE_BUILD_DESC="R800i-user 4.0.3 4.1.H.0.4 -z9-3w test-keys"
 
